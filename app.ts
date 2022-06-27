afb2acc43dd640256b975cdfd78021952d5ff78a
AO2324-00 <ao.2324.00@gmail.com>
2022-06-27 22:55:10 +0900

    importで不要なものを削除

--- app.ts
+++ app.ts
@@ -1,4 +1,4 @@
-import { System, Config, SystemRequest, SystemResponse, getHandlerFunctions, loadRoutingFiles } from "https://github.com/PuddleServer/Puddle/raw/v1.1.2-beta/mod.ts";
+import { System, Config, SystemRequest, SystemResponse} from "https://github.com/PuddleServer/Puddle/raw/v1.1.2-beta/mod.ts";
 
 System.listen("./.env", (conf: Config) => {
     console.log(`The server running on http://${conf.hostname}:${conf.port}`);
