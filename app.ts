3c16726b4999ea4eeca043cef1bad793a3784d3a
johmaru <hatunehimeka@outlook.jp>
2022-06-28 02:16:31 +0900

    MongoDBにユーザーとパスワードを追加する機能

--- app.ts
+++ app.ts
@@ -1,5 +1,36 @@
 import { System, Config, SystemRequest, SystemResponse, getHandlerFunctions, loadRoutingFiles } from "https://github.com/PuddleServer/Puddle/raw/v1.1.2-beta/mod.ts";
+import { config } from "https://deno.land/x/dotenv/mod.ts";
+import {
+    Bson,
+    MongoClient,
+  } from "https://deno.land/x/mongo@v0.30.1/mod.ts";
 
+
+  const ENV_PATH = './.env';
+  const config_env:any = config({ path: ENV_PATH });
+  const user:string = config_env.mongo_user
+  const password:string = config_env.mongo_password
+  const host:string = config_env.mongo_host
+  const port:number = config_env.mongo_port
+  const db_name:string = config_env.mongo_db
+  const client = new MongoClient()
+  await client.connect({
+    db: db_name,
+    tls: true,
+    servers: [
+      {
+        host: host,
+        port: 27017,
+      },
+    ],
+    credential: {
+      username: user,
+      password: password,
+      db: db_name,
+      mechanism: "SCRAM-SHA-1",
+    },
+  });
+  System.setModule(`db`, client)
 System.listen(8080, (conf: Config) => {
     console.log(`The server running on http://${conf.hostname}:${conf.port}`);
 });
