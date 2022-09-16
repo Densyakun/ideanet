f47e141e83e8cb3ebd48674c8cb7b2b129801b9e
densyakun <densyakun1128@gmail.com>
2022-09-17 02:27:17 +0900

    add not connected to mongodb alert

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,49 +2,59 @@ import type { InferGetServerSidePropsType } from 'next'
 import Head from 'next/head'
 import React from "react";
 import { useForm } from "react-hook-form";
-import { Button, Card, Container, Form, Navbar, Stack } from 'react-bootstrap'
+import { Alert, Button, Card, Container, Form, Navbar, Stack } from 'react-bootstrap'
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap-icons/font/bootstrap-icons.css"
 import ScrollShadows from '../components/ScrollShadows'
+import clientPromise from '../lib/mongodb'
 
-type IndexPageData = {
-  posts: {
-    _id: string,
-    text: string
-  }[]
-}
+type PostListData = {
+  _id: string,
+  text: string
+}[]
 
 type Inputs = {
   text: string
 }
 
 export const getServerSideProps = async () => {
-  const data: IndexPageData = {
-    posts: [
-      {
-        _id: "0",
-        text: "目標を達成する"
-      },
-      {
-        _id: "1",
-        text: "社会活動の効率化"
-      },
-      {
-        _id: "2",
-        text: "（手段）リソース（経営資源）を共有するWebアプリの開発"
-      }
-    ]
-  }
-
-  return {
-    props: {
-      data,
+  const allPosts: PostListData = [
+    {
+      _id: "0",
+      text: "目標を達成する"
     },
+    {
+      _id: "1",
+      text: "社会活動の効率化"
+    },
+    {
+      _id: "2",
+      text: "（手段）リソース（経営資源）を共有するWebアプリの開発"
+    }
+  ]
+
+  try {
+    await clientPromise
+
+    return {
+      props: {
+        posts: allPosts,
+        isConnected: true,
+      },
+    }
+  } catch (e) {
+    console.error(e)
+    return {
+      props: {
+        posts: allPosts,
+        isConnected: false,
+      },
+    }
   }
 }
 
-export default function Page({ data }: InferGetServerSidePropsType<typeof getServerSideProps>) {
-  const postCards = data.posts.map((post) =>
+export default function Page({ posts, isConnected }: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  const postCards = posts.map((post) =>
     <Card key={post._id} bg="dark" text="white">
       <Card.Body>
         <Card.Text>
@@ -78,42 +88,51 @@ export default function Page({ data }: InferGetServerSidePropsType<typeof getSer
 
         <ScrollShadows>
           <main className="container-sm py-3">
-            <p>IdeaNetは、社会活動の効率化のために、誰でも匿名で情報を共有できる、スレッド形式の電子掲示板です。</p>
+            {isConnected ? (
+              <>
+                <p>IdeaNetは、社会活動の効率化のために、誰でも匿名で情報を共有できる、スレッド形式の電子掲示板です。</p>
 
-            <p>まずは、あなたの目標を投稿してみましょう。</p>
+                <p>まずは、あなたの目標を投稿してみましょう。</p>
 
-            <h5>投稿する</h5>
+                <h5>投稿する</h5>
 
-            <Card bg="dark" text="white" className="mb-2">
-              <Card.Body>
-                <Form onSubmit={onSubmit}>
-                  <fieldset>
-                    <Form.Group className="mb-3" controlId="formBasicEmail">
-                      <Form.Control
-                        as="textarea"
-                        placeholder="ここにアイデアを書く"
-                        isInvalid={!!errors.text}
-                        rows={2}
-                        {...register("text", { required: true, maxLength: 255 })}
-                      />
-                      <Form.Control.Feedback type="invalid">
-                        {errors.text?.type === 'maxLength' && "テキストは 255 文字以下で入力してください。"}
-                      </Form.Control.Feedback>
-                    </Form.Group>
-                    <Button variant="primary" type="submit">
-                      <i className="bi bi-plus"></i>
-                      投稿する
-                    </Button>
-                  </fieldset>
-                </Form>
-              </Card.Body>
-            </Card>
+                <Card bg="dark" text="white" className="mb-2">
+                  <Card.Body>
+                    <Form onSubmit={onSubmit}>
+                      <fieldset>
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                          <Form.Control
+                            as="textarea"
+                            placeholder="ここにアイデアを書く"
+                            isInvalid={!!errors.text}
+                            rows={2}
+                            {...register("text", { required: true, maxLength: 255 })}
+                          />
+                          <Form.Control.Feedback type="invalid">
+                            {errors.text?.type === 'maxLength' && "テキストは 255 文字以下で入力してください。"}
+                          </Form.Control.Feedback>
+                        </Form.Group>
+                        <Button variant="primary" type="submit">
+                          <i className="bi bi-plus"></i>
+                          投稿する
+                        </Button>
+                      </fieldset>
+                    </Form>
+                  </Card.Body>
+                </Card>
 
-            <h5>みんなの投稿</h5>
+                <h5>みんなの投稿</h5>
 
-            <Stack gap={2}>
-              {postCards}
-            </Stack>
+                <Stack gap={2}>
+                  {postCards}
+                </Stack>
+              </>
+            ) : (
+              <Alert variant="danger" className="mb-0">
+                You are NOT connected to MongoDB. Check the <code>README.md</code>{' '}
+                for instructions.
+              </Alert>
+            )}
           </main>
         </ScrollShadows>
       </div>
